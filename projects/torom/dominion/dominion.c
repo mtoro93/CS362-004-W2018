8e22943c2a9d5b2b545b63f4aa9322d71343734c
Matthew Toro <torom@flip3.engr.oregonstate.edu>
2018-01-21 04:59:46 -0800

    refactored adventurer card

--- projects/torom/dominion/dominion.c
+++ projects/torom/dominion/dominion.c
@@ -667,24 +667,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
+		playAdventurer(&drawntreasure, state, currentPlayer, &cardDrawn, temphand, &z);
       return 0;
 			
     case council_room:
@@ -1328,6 +1311,31 @@ int updateCoins(int player, struct gameState *state, int bonus)
   return 0;
 }
 
-
+void playAdventurer(int *drawntreasure, struct gameState *state, int currentPlayer, int* cardDrawn, int* temphand, int *z)
+{
+	while((*drawntreasure) < 2)
+	{
+		if (state->deckCount[currentPlayer] < 1)
+		{//if the deck is empty we need to shuffle discard and add to deck
+		  shuffle(currentPlayer, state);
+		}
+		drawCard(currentPlayer, state);
+		cardDrawn = &(state->hand[currentPlayer][state->handCount[currentPlayer]-1]);
+		//top card of hand is most recently drawn card.
+		if ((*cardDrawn) == copper || (*cardDrawn) == silver || (*cardDrawn) == gold)
+		  (*drawntreasure)++;
+		else
+		{
+		  temphand[(*z)]=(*cardDrawn);
+		  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+		  (*z)++;
+		}
+    }
+	while(*z-1>=0)
+	{
+		state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[*z-1]; // discard all cards in play that have been drawn
+		*z=*z-1;
+	}
+}
 //end of dominion.c
 
